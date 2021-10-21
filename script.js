695690d603bac7dc764a01af55816865992905ab
Laura <89469591+lfisch00@users.noreply.github.com>
2021-10-20 21:19:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-(function () {
-  "use strict";
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  // Add Code Below Here
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
 
-})();
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+		
+function checkGuess() {
+  alert('I am a placeholder');
+}
